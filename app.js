7ba0ec60c400064072481c073b847c5edb95cc28
Vero Conesa <vero.gconesa@gmail.com>
2023-04-03 13:29:01 +0200

    📚 docs Added dependencies

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ require('dotenv').config()
 const app = express()
 const cors = require('cors')
 const { dbConnection } = require('./db')
+const userRouter = require('./routes/user')
+const taskRouter = require('./routes/task')
 
 app.use(cors())
 app.use(express.json())
 const PORT = process.env.PORT || 4001
+
+app.use('/api', userRouter)
+app.use('/api', taskRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello Wordl!!')
