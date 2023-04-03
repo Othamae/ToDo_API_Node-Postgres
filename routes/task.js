30d0af0a736d8858d64713b6d1f71a599e3bc585
Vero Conesa <vero.gconesa@gmail.com>
2023-04-03 15:39:57 +0200

    ⚡️ perf Added validators to routes

--- routes/task.js
+++ routes/task.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const { getAllTask, getTask, addTask, updateTask, deleteTask } = require('../controllers/task')
+const { validatorCreateTask, validatorUpdateTask } = require('../validators/task')
 
 router.get('/task', getAllTask)
 
 router.get('/tast/:id', getTask)
 
-router.post('/task', addTask)
+router.post('/task', validatorCreateTask, addTask)
 
-router.put('/task/:id', updateTask)
+router.put('/task/:id', validatorUpdateTask, updateTask)
 
 router.delete('/task/:id', deleteTask)
 
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const { userRegister, userLogin } = require('../controllers/user')
 const { getAllTaskByUser } = require('../controllers/task')
+const { validatorLogin, validatorRegister } = require('../validators/user')
 
 const router = express.Router()
 
-router.post('/user/register', userRegister)
+router.post('/user/register', validatorRegister, userRegister)
 
-router.post('/user/login', userLogin)
+router.post('/user/login', validatorLogin, userLogin)
 
 router.get('/user/:id/task', getAllTaskByUser)
 
